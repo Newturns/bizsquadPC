2e6bcac682775bc03f4b120ffe6d3b5b9d79cb0e
opentask <opentask@opentask.co.kr>
2019-11-15 13:26:31 +0900

    progress bar 수정중

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -18,11 +18,11 @@ export class ProgressBarComponent extends TakeUntil implements OnInit{
   }
 
   ngOnInit(): void {
-    if(this.progress$){
-      this.progress$
-        .pipe(this.takeUntil)
-        .subscribe((percentage: number) => this.progress = percentage);
-    }
+    // if(this.progress$){
+    //   this.progress$
+    //     .pipe(this.takeUntil)
+    //     .subscribe((percentage: number) => this.progress = percentage);
+    // }
   }
 
 }
